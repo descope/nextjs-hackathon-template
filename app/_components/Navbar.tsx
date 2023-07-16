6af1d7f0e786bb1751725eae6d7c5ed015c2cbbf
Eric Sheen <eric.sprog@gmail.com>
2023-07-16 12:41:04 -0400

    navbar margin bottom

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -49,7 +49,7 @@ export default function Navbar({ Logo }: { Logo: string }) {
                             <a href="#faq" className="max-md:text-white block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">FAQ</a>
                         </li>
                         <li>
-                            <Link href={"/team"} className="max-md:text-white block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Team</Link>
+                            <Link href={"/team"} className="max-md:text-white block py-2 pl-3 pr-4 max-md:mb-2 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Team</Link>
                         </li>
                         <li className='mr-6 max-md:pb-2'>
                             {session ? 
