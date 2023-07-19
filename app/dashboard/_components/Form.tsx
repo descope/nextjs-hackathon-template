cfc062dd1f4bc242db9fb76c34597af6ea26c441
Eric Sheen <eric.sprog@gmail.com>
2023-07-19 10:58:45 -0400

    embed

--- app/dashboard/_components/Form.tsx
+++ app/dashboard/_components/Form.tsx
@@ -2,7 +2,7 @@ export default function Form() {
     return (
         <div className="col">
             <p className="text-lg mb-4"><span className="text-pink-500">NOTE!</span> Use the same email that you have logged in with</p>
-            <iframe className="w-full h-screen" src="https://airtable.com/embed/shr7474wHFJjkJwbM?backgroundColor=red"></iframe>
+            <iframe className="w-full h-screen" src={process.env.AIRTABLE_FORM_EMBED}></iframe>
         </div>
     )
 }
