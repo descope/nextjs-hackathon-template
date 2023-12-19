a87170067c28cf891c24ffc80af093c3ed9709e6
Kevin Gao <kevinjamesgao@gmail.com>
2023-12-19 15:16:24 -0800

    added new files

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <div className="row justify-between mb-8">
       <h1 className="title mb-0">Hacker Dashboard</h1>
       <button
         className="bg-red-500 text-white py-3 px-6 max-md:mt-6"
         onClick={async () => {
-          await signOut();
           window.location.href = `/api/auth/federated-sign-out`;
+          await signOut({ redirect: true }).then(() => {
+            window.location.href = "/";
+          });
         }}
       >
         Sign Out
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,46 @@
-import './globals.css'
+import "./globals.css";
 
-import { Silkscreen } from 'next/font/google'
+import { Silkscreen } from "next/font/google";
 
-import NextAuthSessionProvider from './_providers/sessionProvider'
-import Navbar from './_components/Navbar'
+import NextAuthSessionProvider from "./_providers/sessionProvider";
+import Navbar from "./_components/Navbar";
 
-import LogoBlack from "./_assets/logo/icons8-pixel-heart-100 (black).png"
-import LogoWhite from "./_assets/logo/icons8-pixel-heart-100.png"
+import LogoBlack from "./_assets/logo/icons8-pixel-heart-100 (black).png";
+import LogoWhite from "./_assets/logo/icons8-pixel-heart-100.png";
 
-import Bottom from './_components/Bottom'
-import { SocialList } from './_template_data/Social'
-
+import Bottom from "./_components/Bottom";
+import { SocialList } from "./_template_data/Social";
+import { AuthProvider } from "@descope/react-sdk";
 
 const silkScreen = Silkscreen({
-    subsets: ["latin"],
-    style: "normal",
-    weight: '400',
-    display: 'swap'
-})
-
+  subsets: ["latin"],
+  style: "normal",
+  weight: "400",
+  display: "swap",
+});
 
 export const metadata = {
   icons: {
-    icon: '/descope.jpeg',
+    icon: "/descope.jpeg",
   },
-  title: 'Next.js Hackathon Template with Descope Next-Auth',
-  description: 'Created by Descope',
-}
+  title: "Next.js Hackathon Template with Descope Next-Auth",
+  description: "Created by Descope",
+};
 
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={silkScreen.className}>
-          <NextAuthSessionProvider>
-            <Navbar Logo={LogoBlack.src} />
-            <div>
-              {children}
-            </div>
-            <Bottom Logo={LogoWhite.src} SocialList={SocialList}  />
-          </NextAuthSessionProvider>
+        <NextAuthSessionProvider>
+          <Navbar Logo={LogoBlack.src} />
+          <div>{children}</div>
+          <Bottom Logo={LogoWhite.src} SocialList={SocialList} />
+        </NextAuthSessionProvider>
       </body>
     </html>
-  )
+  );
 }
-
-
