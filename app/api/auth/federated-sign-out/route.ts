765812494282e975af9f37a7c6f7da7ef94642df
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-01-05 12:05:29 -0800

    Update route.ts

--- app/api/auth/federated-sign-out/route.ts
+++ app/api/auth/federated-sign-out/route.ts
@@ -1,7 +1,6 @@
 import { authOptions } from "@/app/_utils/options";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { signOut } from "next-auth/react";
 
 export const dynamic = "force-dynamic";
 
@@ -12,16 +11,17 @@ const handler = async (req: NextRequest, res: NextResponse) => {
       return NextResponse.redirect(process.env.NEXTAUTH_URL!);
     }
 
-    const endSessionURL = `https://api.descope.com/oauth2/v1/logout`;
-    const redirectURL = `${process.env.NEXTAUTH_URL}/signout`;
-    const endSessionParams = new URLSearchParams({
-      // @ts-ignore
-      id_token_hint: session.idToken,
-      post_logout_redirect_uri: redirectURL,
-    });
-    const fullUrl = `${endSessionURL}?${endSessionParams.toString()}`;
+    const res = fetch("https://api.descope.com/oauth2/v1/logout", {
+      method: "POST",
+      body: new URLSearchParams({
+        // @ts-ignore
+        id_token_hint: session.idToken,
+        post_logout_redirect_uri: process.env.NEXTAUTH_URL!,
+      }),
+    })
 
-    return NextResponse.redirect(fullUrl);
+    return NextResponse.json({ success: true });
+
   } catch (error) {
     console.error(error);
   }
