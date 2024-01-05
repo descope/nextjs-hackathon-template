7b3463112f88b37efdfe28b518ea6c3aab38585e
Kevin Gao <kevinjamesgao@gmail.com>
2024-01-05 11:57:02 -0800

    added changes

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <div className="row justify-between mb-8">
       <h1 className="title mb-0">Hacker Dashboard</h1>
@@ -10,7 +13,10 @@ export default function Header() {
         className="bg-red-500 text-white py-3 px-6 max-md:mt-6"
         onClick={async () => {
           try {
-            window.location.href = "/api/auth/federated-sign-out";
+            await fetch("/api/auth/federated-sign-out").then(() => {
+              signOut({ redirect: false });
+              router.push("/");
+            });
           } catch (error) {
             console.error("Error during sign out:", error);
           }
